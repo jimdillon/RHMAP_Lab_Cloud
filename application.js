d33fb1dac982e35080f2751426d2471ccf62996d
Jim Dillon <jdillon@jdillon.remote.csb>
2015-09-22 22:33:28 -0400

    initial

--- application.js
+++ application.js
@@ -1,38 +1,33 @@
-var mbaasApi = require('fh-mbaas-api');
-var express = require('express');
-var mbaasExpress = mbaasApi.mbaasExpress();
-var cors = require('cors');
+'use strict';
+var mbaasApi = require('fh-mbaas-api')
+, express = require('express')
+, mbaasExpress = mbaasApi.mbaasExpress()
+, log = require('fhlog')
+, app = express();
 
-// list the endpoints which you want to make securable here
-var securableEndpoints;
-// fhlint-begin: securable-endpoints
-securableEndpoints = ['/hello'];
-// fhlint-end
-
-var app = express();
-
-// Enable CORS for all requests
-app.use(cors());
 
 // Note: the order which we add middleware to Express here is important!
-app.use('/sys', mbaasExpress.sys(securableEndpoints));
+app.use('/sys', mbaasExpress.sys([]));
 app.use('/mbaas', mbaasExpress.mbaas);
-
-// allow serving of static files from the public directory
-app.use(express.static(__dirname + '/public'));
 
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
 // fhlint-begin: custom-routes
-app.use('/hello', require('./lib/hello.js')());
+app.use('/weather', require('./lib'));
 // fhlint-end
 
-// Important that this is last!
-app.use(mbaasExpress.errorHandler());
+mbaasExpress.errorHandler()
+
+app.use(function errorHandler (err, req, res, next) {
+  log.e('Received error: %s', err);
+  log.e(err.stack);
+
+  res.send(500, 'Internal Server Error');
+});
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
+  console.log("App started at: " + new Date() + " on port: " + port);
 });
